b4c6a41068fdc7332873e5da20b4046f9d8890f2
mreanew <mreanew@126.com>
2017-06-24 18:33:23 +0800

    import code

---
+++ webapp/Application.js
@@ -0,0 +1,433 @@
+/****************************************************************************
+ * DESC       Application
+ * AUTHOR     zhouxianli
+ * CREATEDATE 2003-05-04
+ * MODIFYLIST    Name       Date            Reason/Contents
+ *          ------------------------------------------------------
+ *
+ ************************************************************************************/
+ 
+
+function formatFloat(value,count,precision,delimiterChar)
+{
+  count = count==null?3:count;
+  precision = precision==null?2:precision;
+  delimiterChar = delimiterChar==null?",":delimiterChar;
+
+  var strMinus = "";
+  if(value<0)
+  {
+    strMinus = "-";
+    value = -1*value;
+  }
+
+  var strReturn = "";
+  var strValue = point(round(value,precision),precision);
+
+  strReturn = strValue.substring(strValue.length-precision-1);
+  strValue = strValue.substring(0,strValue.length-precision-1);
+  while(strValue.length>count)
+  {
+    strReturn = delimiterChar + strValue.substring(strValue.length-count) + strReturn;
+    strValue = strValue.substring(0,strValue.length-count);
+  }
+
+  strReturn = strMinus + strValue + strReturn;
+  return strReturn;
+}  
+function pointTwo( s )
+{
+  return point(s,2);
+}
+
+function pointFour( s )
+{
+  return point(s,4);
+}
+
+
+
+//replace enter to tab
+/*
+function onkeydownHander(){
+  if(   event.keyCode==13
+     && event.srcElement.type!='button'
+     && event.srcElement.type!='submit'
+     && event.srcElement.type!='reset'
+     && event.srcElement.type!='textarea')
+
+     event.keyCode=9;
+}
+document.attachEvent("onkeydown",onkeydownHander);
+*/
+
+
+function functionReturnFalse()
+{
+  return false;
+}
+
+function functionReturnTrue()
+{
+  return true;
+}
+
+function functionDoNothing()
+{
+  //do nothing
+}
+
+function functionCancelFocus()
+{
+  this.blur();
+  window.focus();
+  return false;
+}
+
+function isDate(date,splitChar)
+{
+  var charSplit = (splitChar==null?"-":splitChar);
+  var strValue = date.split(charSplit);
+
+  if(strValue.length!=3) return false;
+  if(!isInteger(strValue[0]) || !isInteger(strValue[1]) || !isInteger(strValue[2]) ) return false;
+
+  var intYear  = parseInt(strValue[0],10);
+  var intMonth = parseInt(strValue[1],10)-1;
+  var intDay   = parseInt(strValue[2],10);
+
+  var dt = new Date(intYear,intMonth,intDay);
+  if( dt.getFullYear() != intYear ||
+      dt.getMonth() != intMonth ||
+      dt.getDate() != intDay
+     )
+  {
+    return false;
+  }
+  return true;
+}
+
+/*初步认定为
+MD D 判断日期差异； 为 M 判断月份差异
+*/
+function dateDiff(dateStart,dateEnd,MD)
+{
+  if(MD=="D")
+  {
+    var endTm = dateEnd.getTime();
+    var startTm = dateStart.getTime();
+    var diffDay = (endTm-startTm)/86400000+1;
+
+    var diffDayTemp = "'" + diffDay + "'";
+    if(diffDayTemp.indexOf(".") != -1){
+      diffDay = parseInt(parseInt(diffDay, 10) + 1, 10);
+    }
+
+    return diffDay;
+  }
+  else
+  {
+    var endD = dateEnd.getDate();
+    var endM = dateEnd.getMonth();
+    var endY = dateEnd.getFullYear();
+    var startD = dateStart.getDate();
+    var startM = dateStart.getMonth();
+    var startY = dateStart.getFullYear();
+
+    if(endD>=startD)
+    {
+      return (endY-startY)*12+(endM-startM)+1;
+    }
+    else
+    {
+      return (endY-startY)*12+(endM-startM);
+    }
+  }
+}
+
+function isLeapYear(strCheckYear)
+{
+  var check4=strCheckYear%4==0?1:0;
+  var check100=strCheckYear%100==0?-1:0;
+  var check400=strCheckYear%400==0?1:0;
+  var result=check4+check100+check400;
+  if(result==1)
+  {
+    return true
+  }
+  return false
+}
+ 
+
+
+function getNextDateFullDate(strDate,intCount)
+{
+  var tempDate = new Date(replace(strDate,"-","/"));
+  if(intCount == null)
+  {
+    intCount =1;
+  }
+
+  var nextDateInMS = tempDate.getTime() + (intCount * 24 * 60 * 60 * 1000 );
+  var strReturn = convertFullDateToString(new Date(nextDateInMS));
+  return strReturn;
+}
+
+function getNextMonthFullDate(strDate,intCount)
+{
+  var tempDate = new Date(replace(strDate,"-","/"));
+  if(intCount == null)
+  {
+    intCount =1;
+  }
+
+  tempDate.setMonth(tempDate.getMonth() + intCount );
+  var strReturn = convertFullDateToString(tempDate);
+  return strReturn;
+}
+
+function getNextYearFullDate(strDate,intCount)
+{
+  var tempDate = new Date(replace(strDate,"-","/"));
+  if(intCount == null)
+  {
+    intCount =1;
+  }
+
+  tempDate.setFullYear(tempDate.getFullYear() + intCount );
+  var strReturn = convertFullDateToString(tempDate);
+  return strReturn;
+}
+
+function convertFullDateToString(date) {
+  if(date==null) {
+    date = new Date();
+  }
+
+  var strDate = "";
+  var year = "";
+  var month = "";
+  var day = "";
+  year = date.getFullYear();
+  if(parseInt(date.getMonth() + 1, 10) < 10) {
+    month = "0" + parseInt(date.getMonth() + 1, 10)
+  } else {
+    month = parseInt(date.getMonth() + 1, 10);
+  }
+  if(parseInt(date.getDate(), 10) < 10) {
+    day = "0" + parseInt(date.getDate(), 10)
+  } else {
+    day = parseInt(date.getDate(), 10);
+  }
+  strDate = year + DATE_DELIMITER + month + DATE_DELIMITER + day;
+  return strDate;
+}
+
+function isNumeric(strValue)
+{
+  var result = regExpTest(strValue,/\d*[.]?\d*/g);
+  return result;
+}
+
+function isInteger(strValue)
+{
+  var result = regExpTest(strValue,/\d+/g);
+  return result;
+}
+
+function checkFullDate(field)
+{
+  field.value = trim(field.value);
+  var strValue = field.value;
+  if(strValue=="")
+  {
+    return false;
+  }
+  if(isNumeric(strValue))
+  {
+    if(strValue.length > 6 && strValue.length < 9)
+    {
+      strValue = strValue.substring(0,4) + DATE_DELIMITER + strValue.substring(4,6) + DATE_DELIMITER + strValue.substring(6);
+      field.value = strValue;
+    }
+    else
+    {
+      errorMessage("The correct date format is yyyy-MM-dd.");
+      field.value="";
+      field.focus();
+      field.select();
+      return false;
+    }
+  }
+  if( !isDate(strValue,DATE_DELIMITER) && !isDate(strValue)||strValue.substring(0,1)=="0")
+  {
+    errorMessage("The correct date format is yyyy-MM-dd.");
+    field.value="";
+    field.focus();
+    field.select();
+    return false;
+  }
+  return true;
+}
+
+function compareFullDate(date1,date2)
+{
+  var strValue1=date1.split(DATE_DELIMITER);
+  var date1Temp=new Date(strValue1[0],parseInt(strValue1[1],10)-1,parseInt(strValue1[2],10));
+
+  var strValue2=date2.split(DATE_DELIMITER);
+  var date2Temp=new Date(strValue2[0],parseInt(strValue2[1],10)-1,parseInt(strValue2[2],10));
+
+  if(date1Temp.getTime()==date2Temp.getTime())
+    return 0;
+  else if(date1Temp.getTime()>date2Temp.getTime())
+    return 1;
+  else
+    return -1;
+}
+
+
+/**********************************************/
+/************* Loading Bar  *******************/
+/**********************************************/
+/** deleteNode when page load*/
+if (window.attachEvent) {   
+   window.attachEvent("onload", delLoadingNode);   
+} else if (window.addEventListener) {   
+   window.addEventListener("load", delLoadingNode, false);    
+}
+/** deleteNode */
+function  delLoadingNode(){   
+  var nodeId = "loading";
+  try{   
+	  var div =document.getElementById(nodeId);  
+	  if(div !==null){
+		  document.body.removeChild(div);
+		  div=null;    
+ 	  }  
+  } catch(e){   
+  	alert("delete node "+nodeId+" error");
+  }   
+}
+
+
+// changestyle.js
+
+
+function ExChgClsName(Btn,Obj){
+	var obj=document.getElementById(Obj);
+		obj.style.display =obj.style.display == "none" ? "" : "none";
+if(obj.style.display==""){
+		Btn.className='default';
+   }else{
+   Btn.className='down'
+}		
+}
+
+function addLoadEvent(func) {
+			var oldonload = window.onload;
+			
+			if (typeof window.onload != "function") {
+				window.onload = func;
+			} else {
+				window.onload = function () {
+					oldonload();
+					func();
+				}
+			}
+		}
+		
+		/*------------------------------------+
+		 | Functions to run when window loads |
+		 +------------------------------------*/
+		addLoadEvent(function () {
+			initChecklist();
+			diffent();
+		});
+		
+		/*----------------------------------------------------------+
+		 | initChecklist: Add :hover functionality on labels for IE |
+		 +----------------------------------------------------------*/
+		function initChecklist() {
+			if (document.all && document.getElementById) {
+				// Get all unordered lists
+				var lists = document.getElementsByTagName("input");
+				
+				for (i = 0; i < lists.length; i++) {
+					var theList = lists[i];
+					
+					// Only work with those having the class "checklist"
+					if (theList.className.indexOf("upload") > -1 ) {
+
+							theList.onmouseover = function() { this.className="upload_over"; };
+							theList.onmouseout = function() { this.className="upload"; };
+
+					}
+					if (theList.className.indexOf("download") > -1 ) {
+
+							theList.onmouseover = function() { this.className="download_over"; };
+							theList.onmouseout = function() { this.className="download"; };
+
+					}
+					if (theList.className.indexOf("btn_refresh") > -1 ) {
+
+							theList.onmouseover = function() { this.className="btn_refresh_over"; };
+							theList.onmouseout = function() { this.className="btn_refresh"; };
+
+					}
+					if (theList.className.indexOf("btn_zoom") > -1 ) {
+
+							theList.onmouseover = function() { this.className="btn_zoom_over"; };
+							theList.onmouseout = function() { this.className="btn_zoom"; };
+
+					}
+					if (theList.className.indexOf("btn_print") > -1 ) {
+
+							theList.onmouseover = function() { this.className="btn_print_over"; };
+							theList.onmouseout = function() { this.className="btn_print"; };
+
+					}
+					if (theList.className.indexOf("button_ty") > -1 ) {
+
+							theList.onmouseover = function() { this.className="button_ty_over"; };
+							theList.onmouseout = function() { this.className="button_ty"; };
+
+					}
+				}
+			}
+		}
+		
+function diffent(){
+			var Ps = document.getElementsByTagName("p");
+			for (i = 0; i < Ps.length; i++) {
+			var theP = Ps[i];
+            if (theP.className.indexOf("bd_out") > -1 ) {
+							theP.onmouseover = function() { this.className="bd_over"; };
+							theP.onmouseout = function() { this.className="bd_out"; };
+					}
+}
+}
+
+function checkedCheckBox(values,TreeLength,vGroupTree,userOrGroup){
+  var value = new Array();
+  value = values.split(",");
+  for(var i=0;i<value.length;i++){//页面输入域的值 
+    var checkValue = value[i];
+	for(j=0;j<TreeLength;j++){//列表显示的值
+	  if(userOrGroup == "user"){
+		if(trim(checkValue) == trim(vGroupTree.data[j].userCode)){
+		  fm.treeCheckBox[j+1].checked=true;
+		}
+	  }else{
+		var id = vGroupTree.data[j].id;
+		if(checkValue == vGroupTree.data[j].id){
+		  fm.treeCheckBox[j+1].checked=true;
+		}
+	  }
+	}
+  }
+}	
+
+
+
+
---
+++ webapp/calcImp.js
@@ -0,0 +1,341 @@
+
+// 文件名：calcImp.js
+var FULL_YEAR_DAYS_IN_FUND = 365; //基金行业的1年默认年化收益天数为365天
+var rate1,days1;
+var fundNum = 0; //理财产品 序号；
+
+//增加理财产品
+function addFund(){
+	// console.log(document.getElementById('fm1'));
+	fundNum++;
+	var newNode = document.createElement('div');
+	var tempHTML = '<label>产品:1</label>投入资金<input id = "moneyIn:1" name = "moneyIn:1" type="text"/>'+
+	'利率(如0.03 年化利率3%)<input id = "rate:1" name = "rate:1" type="text"/>'+
+	'持有天数<input id = "days:1" name = "days:1" type="text"/>'+
+	'<label>按日复利计算</label><input id = "isCalcDayByDay:1" type = "checkbox" />'+
+	'</div><input value = \'delete\' type = "button" onclick = "deleteRow(this)" />'
+	
+	newNode.innerHTML = tempHTML.replace(/:1/g,fundNum); //将:1替换为真正数字；
+	// fm1.appendChild(newNode);  //在表单内部的结尾，增加一个子节点
+	fm1.insertBefore(newNode,document.getElementById('calcButton'));  //在表单内部的结尾，增加一个子节点
+	
+}
+
+function changeNotice () {	
+	var cfTxtTime = document.getElementById('confirmTime').value;
+	document.getElementById('noticeText').innerText = '基金行业工作天一至五'+ cfTxtTime + 
+	'前转入确认当天份额，'+ cfTxtTime +'点后转入确认T+1天份额。周五'+ cfTxtTime +'后~下周一'
+	+ cfTxtTime +' 前，确认周一的份额;';
+	}
+// window.onload = addFund();
+~function (){
+	addFund();
+	changeNotice();
+	//注册句柄事件
+	document.getElementById('confirmTime').onchange = changeNotice;
+}();
+
+
+
+/* 计算收益函数 */
+function calc() {
+	var revenue = Number(0);
+	var showResult = document.getElementById("resultDiv").getElementsByTagName('p')[0];
+
+	var stringResult ='';
+// 	showResult.innerHTML = null;
+// 	var stringResult  = showResult.innerHTML;
+	var brString = '\r\n';
+	brString = '</br>';
+	for(var i=1; i<=fundNum; i++){
+		if(!document.getElementById('moneyIn'+i)){ continue;}  //不存在，则跳出本次循环；
+		moneyIn = Number(document.getElementById('moneyIn'+i).value); //本金
+		rate = Number(document.getElementById('rate'+i).value); // 0.03 年化利率3%
+		days = Number(document.getElementById('days'+i).value);
+		isCalcDayByDay =document.getElementById('isCalcDayByDay'+i).checked; //是否按日计算复利
+		
+		if(isCalcDayByDay){  //复利计算逻辑
+			stringResult +='产品'+i+'&nbsp;&nbsp;，按日复利，收益后本+息:';
+			var lastMoneyIn = moneyIn; //上期本金
+			var fundRevenue = 0; //累计利息
+
+			while(days>0){
+			    var fundRevenue1Day
+			    fundRevenue1Day = lastMoneyIn*rate/FULL_YEAR_DAYS_IN_FUND  //每天利息
+				lastMoneyIn = lastMoneyIn + fundRevenue1Day;
+				fundRevenue += fundRevenue1Day; //计算累计利息
+				days--;
+			}
+		}else{ //单利计算逻辑
+			stringResult +='产品'+i+'&nbsp;&nbsp;，收益后本+息:';
+			//  列出公式
+			stringResult += moneyIn+'+'+moneyIn+"*"+rate+'/'+FULL_YEAR_DAYS_IN_FUND+'*'+days+'='; 
+			var fundRevenue = moneyIn*rate/FULL_YEAR_DAYS_IN_FUND*days;  //利息收入
+		}
+        fundRevenue = Number(fundRevenue.toFixed(2)); //四舍五入，保留2位小数
+        stringResult +='  '+ (moneyIn+fundRevenue)+ ' ,其中，本金为:'+moneyIn+'  ,'+'利息为： '+fundRevenue ;
+        stringResult +=brString;
+	    
+	    // https://segmentfault.com/a/1190000000324193 网友提供的修复浮点型计算差异问题方案
+// 	    revenue = revenue.add(fundRevenue);  
+		revenue +=  fundRevenue;
+	}
+
+	//解决浮点数相加不准确问题  参考  http://www.iteye.com/problems/71491
+	revenue = revenue.toFixed(2);  
+	//输出总收益
+	stringResult +='总收益：            '+revenue;
+	showResult.innerHTML = stringResult; //只能识别html代码 作换行符、空格；
+// 	showResult.innerText = stringResult;  //能识别换行符，多个空格会缩成一个字符
+// 	showResult.textContent = stringResult;  // 换行符、br都不能识别，多个空格会缩成一个字符；
+	console.log(stringResult);
+}
+
+/* 删除行数 */
+function deleteRow(dom){
+	console.log(dom);
+	var divdom = dom.parentNode; //div
+	fm1.removeChild(divdom);
+}
+
+
+	/**
+ ** 加法函数，用来得到精确的加法结果
+ ** 说明：javascript的加法结果会有误差，在两个浮点数相加的时候会比较明显。这个函数返回较为精确的加法结果。
+ ** 调用：accAdd(arg1,arg2)
+ ** 返回值：arg1加上arg2的精确结果
+ **/
+function accAdd(arg1, arg2) {
+    var r1, r2, m, c;
+    try {
+        r1 = arg1.toString().split(".")[1].length;
+    }
+    catch (e) {
+        r1 = 0;
+    }
+    try {
+        r2 = arg2.toString().split(".")[1].length;
+    }
+    catch (e) {
+        r2 = 0;
+    }
+    c = Math.abs(r1 - r2);
+    m = Math.pow(10, Math.max(r1, r2));
+    if (c > 0) {
+        var cm = Math.pow(10, c);
+        if (r1 > r2) {
+            arg1 = Number(arg1.toString().replace(".", ""));
+            arg2 = Number(arg2.toString().replace(".", "")) * cm;
+        } else {
+            arg1 = Number(arg1.toString().replace(".", "")) * cm;
+            arg2 = Number(arg2.toString().replace(".", ""));
+        }
+    } else {
+        arg1 = Number(arg1.toString().replace(".", ""));
+        arg2 = Number(arg2.toString().replace(".", ""));
+    }
+    return (arg1 + arg2) / m;
+}
+
+//给Number类型增加一个add方法，调用起来更加方便。
+Number.prototype.add = function (arg) {
+    return accAdd(arg, this);
+};
+
+
+// var weekDayNum = [0,1,2,3,4,5,6]; // 6，0分别指周六、周日
+// var workdayNum = [1,2,3,4,5] ;// 6，0分别指周六、周日
+var weekendDayNum = [0,6]; //周末
+// var nomalTPlus0DayNum = [1,2,3,4]; // 一般计算逻辑 1、2、3、4 周1~周4; 
+
+/* 计算转入时间的实际计算收益的第一天
+*/
+function calcInDateTime(){
+	console.log("enter calcInDateTime()");
+	var intime = new Date(document.getElementById('inTime').value);
+	//测试代码 -begin
+	if(arguments.length>0){intime =new Date( arguments[0]); }
+	//测试代码 -end
+	var calMoneyDate = null;      //确认份额日期
+	var comfirmType = getRadioBoxValue('comfirmWay');  //确认份额的计算类型； 
+	/*   T+1 计算收益起始日逻辑 1、15点前 当前工作日确认份额，下一个工作日获得收益；
+			 2、15点后 T+1个工作日确认份额，T+2个工作日获得收益；
+			 3、周末转入，（最近的工作日N）周1确认份额，（N日+1工作日）周2获得收益 (与逻辑1 可以归纳为同一个情况)
+
+		 T+0 计算收益起始日逻辑 
+		     1、15点前 当前工作日T确认份额，T工作日获得收益；
+			 2、15点后 T+1工作日确认份额， T+1工作日获得收益；
+			 3、周末转入，（最近的工作日N）周1确认份额+获得收益 (与逻辑1 可以归纳为同一个情况)
+
+			  */
+// 	switch()  // 判断是什么计算方式
+	
+			
+	calMoneyDate = new Date(intime.toDateString());
+	//确认份额日期截止时间 ；
+	var time = document.getElementById('confirmTime').value.split(':');
+	var confirmTime = new Date(intime).setHours(time[0],time[1],time[2]); //返回毫秒数
+
+	if(comfirmType == 'T+1'){
+		//t+1 方式
+		if(intime.getTime() < confirmTime || containsInArray(weekendDayNum , calMoneyDate.getDay())){ 
+			//15点之前 or //是否weekend
+			calMoneyDate = postponeToNextWorkDay(calMoneyDate);
+		} else{ //15点之后
+			calMoneyDate = postponeToNextWorkDay(calMoneyDate);
+			calMoneyDate = postponeToNextWorkDay(calMoneyDate);
+			 } 		
+	} else if(comfirmType == 'T+0'){
+		//t+0 方式
+		if(intime.getTime() < confirmTime || containsInArray(weekendDayNum , calMoneyDate.getDay())){ 
+			//15点之前 or //是否weekend
+			calMoneyDate = postponeFromWeekendToWorkDay(calMoneyDate);
+		} else{ //15点之后
+			calMoneyDate = postponeToNextWorkDay(calMoneyDate);
+		} 	
+	}
+	console.log("intime");
+	console.log(intime);
+	console.log(calMoneyDate);
+	console.log("leave calcInDateTime()");
+// 	intime.setHours(15,0,0,0); //计算  比较是15点前还是后；
+}
+
+/*
+计算转出时间截止实际收益的那一天
+*/
+function calcOutDateTime(){
+	console.log("enter calcInDateTime()");
+	var outtime = new Date(document.getElementById('outTime').value);
+	//测试代码 -begin
+	if(arguments.length>0){outtime =new Date( arguments[0]); }
+	//测试代码 -end
+	var calMoneyDate = null;      //收益截止日
+	var leaveType = getRadioBoxValue('leaveWay');  //确认份额的计算类型； 
+	/*   R+0 百度活期盈/快速取出(资金当天到账) 取出资金部分当天没有收益；
+		R+1  百度定活盈/普通取出，资金下一个工作天到账。收益截止到下一个工作天的前一天；
+			  */
+// 	switch()  // 判断是什么计算方式
+	
+			
+	calMoneyDate = new Date(outtime.toDateString());
+	//确认份额日期截止时间 ；
+	var time = document.getElementById('confirmTime').value.split(':');
+	var confirmTime = new Date(outtime).setHours(time[0],time[1],time[2]); //返回毫秒数
+
+	if(leaveType == 'R+1'){
+		//t+1 方式
+		if(intime.getTime() < confirmTime || containsInArray(weekendDayNum , calMoneyDate.getDay())){ 
+			//15点之前 or //是否weekend
+			calMoneyDate = postponeToNextWorkDay(calMoneyDate);
+		} else{ //15点之后
+			calMoneyDate = postponeToNextWorkDay(calMoneyDate);
+			calMoneyDate = postponeToNextWorkDay(calMoneyDate);
+			 } 		
+	} else if(leaveType == 'R+0'){
+		//t+0 方式
+		if(outtime.getTime() < confirmTime || containsInArray(weekendDayNum , calMoneyDate.getDay())){ 
+			//15点之前 or //是否weekend
+			calMoneyDate = postponeFromWeekendToWorkDay(calMoneyDate);
+		} else{ //15点之后
+			calMoneyDate = postponeToNextWorkDay(calMoneyDate);
+		} 	
+	}
+	console.log("outtime");
+	console.log(outtime);
+	console.log(calMoneyDate);
+	console.log("leave calcInDateTime()");
+// 	intime.setHours(15,0,0,0); //计算  比较是15点前还是后；
+}
+
+/**
+ 找到下一个工作日
+*/
+function postponeToNextWorkDay(cdate){
+	var ncdate = cdate;
+	ncdate = postponeFromWeekendToWorkDay(ncdate); //判断是否weekend ，是则推到第一个工作天
+	ncdate.setDate(ncdate.getDate()+1); //找到nextday
+	ncdate = postponeFromWeekendToWorkDay(ncdate); //再判断是否weekend ，是则推到第一个工作天
+	return ncdate;
+}
+/**
+
+ 非工作日的，找到下一个工作日(周1)
+ 判断是否周六日，否则，累加到下一个工作日(周1)
+ cdate ---calcdate
+*/
+function postponeFromWeekendToWorkDay(cdate) {
+// 	for(var i = 0; i<weekendDayNum.length; i++){
+// 		if(cdate.getDay() == weekendDayNum[i]){
+// 			cdate.setDate(cdate.getDate()+1);
+// 			postponeFromWeekendToWorkDay(cdate);  // 递归 判读；
+// 		}
+// 	}
+	//判断是否周六日，否则，累加到工作日
+	if(containsInArray(weekendDayNum , cdate.getDay())){  
+			cdate.setDate(cdate.getDate()+1);
+			postponeFromWeekendToWorkDay(cdate);  // 递归 ；
+	} 
+	
+	return cdate;
+}
+
+//获取 checkbox
+function   getRadioBoxValue(radioName) 
+{ 
+            var obj = document.getElementsByName(radioName);  //这个是以标签的name来取控件
+                 for( var i=0; i<obj.length;i++)    {
+
+                  if(obj[i].checked)    { 
+                          return   obj[i].value; 
+                  } 
+              }         
+             return "undefined";       
+}
+
+/**
+	判断简单类型 obj 是否存在于数组 Array；
+**/
+function containsInArray(array , obj){
+	var i = array.length;
+	while(i--){
+		if(array[i] == obj){
+			return true;
+		}
+	}
+	return false;
+}	
+
+
+
+/*
+// 测试开始
+//测试数据 时间
+var intimeArray = [
+'2017-06-19 14:38:27',
+'2017-06-20 14:38:27',
+'2017-06-21 14:38:27',
+'2017-06-22 14:38:27',
+'2017-06-23 14:38:27',
+'2017-06-24 14:38:27',
+'2017-06-25 14:38:27',
+
+'2017-06-19 15:38:27',
+'2017-06-20 15:38:27',
+'2017-06-21 15:38:27',
+'2017-06-22 15:38:27',
+'2017-06-23 15:38:27',
+'2017-06-24 15:38:27',
+'2017-06-25 15:38:27' ];
+
+// console.log(intimeArray);
+//轮询 验证 是否正确；
+for (var i = 0; i < intimeArray.length ;i++){
+    console.log(i);
+//     console.log(intimeArray[i]);
+    var str = intimeArray[i];
+    calcInDateTime(str);
+}
+
+console.log('end');
+*/
